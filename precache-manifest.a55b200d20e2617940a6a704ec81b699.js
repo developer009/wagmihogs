db20dd12f3db9194946dd5375b9c707ae913e33f
developer009 <64910951+developer009@users.noreply.github.com>
2021-12-19 01:16:03 +0530

    Add files via upload

---
+++ precache-manifest.a55b200d20e2617940a6a704ec81b699.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b94d7b88949b8462bd1",
+    "url": "/static/css/main.338f9e73.chunk.css"
+  },
+  {
+    "revision": "6b94d7b88949b8462bd1",
+    "url": "/static/js/main.6b94d7b8.chunk.js"
+  },
+  {
+    "revision": "7c8b5bea1f9898827a9c",
+    "url": "/static/js/1.7c8b5bea.chunk.js"
+  },
+  {
+    "revision": "eeb22aef970e90237cb8",
+    "url": "/static/js/2.eeb22aef.chunk.js"
+  },
+  {
+    "revision": "0887d991b29ea046a41f",
+    "url": "/static/js/runtime~main.0887d991.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  },
+  {
+    "revision": "330d229a4f2c57bf10e7c2d492544676",
+    "url": "/index.html"
+  }
+];
